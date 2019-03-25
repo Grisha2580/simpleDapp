4e28c115bf10d7c33a8c0d4f6a2969fe611b1413
wstenzel <wills.stenzel@gmail.com>
2019-03-25 12:05:18 -0400

    add betting contract to migrations

--- oracleSmartContracts/migrations/2_deploy_contracts.js
+++ oracleSmartContracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-const WeatherOracle = artifacts.require("WeatherOracle")
+const WeatherOracle = artifacts.require("WeatherOracle");
+const BettingContract = artifacts.require("Betting");
 
 module.exports = function(deployer) {
-  deployer.deploy(WeatherOracle, address);
+  deployer.deploy(WeatherOracle).then(function() {
+    return deployer.deploy(BettingContract, WeatherOracle.address)
+  });
 };
